17e22fb7e89e24aef9056636e112382c5ad22925
Ioan Chera <ioan_chera@yahoo.com>
2014-01-19 15:23:59 +0200

    * Added a custom layout for the OpenActivity, which contains a progress indicator with info text * Now finding Wolf3D folders is done asynchronously. Unfortunately it takes too long anyway, so it should only be done on user's demand.

--- src/com/ichera/wolfviewer/OpenActivity.java
+++ src/com/ichera/wolfviewer/OpenActivity.java
@@ -7,14 +7,16 @@ import java.util.ArrayList;
 
 import android.app.ListActivity;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.BaseAdapter;
-import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 /**
@@ -24,8 +26,12 @@ import android.widget.TextView;
  */
 public class OpenActivity extends ListActivity 
 {
-	private LayoutInflater	m_inflater;
-	private OpenAdapter		m_adapter;
+	private LayoutInflater		m_inflater;
+	private OpenAdapter			m_adapter;
+	private ProgressBar			m_progressWheel;
+	private TextView			m_progressText;
+	private boolean				m_dataLoaded;
+	private DirFindAsyncTask	m_dirFindAsyncTask;
 	
 	/**
 	 * Adapter for this list
@@ -100,50 +106,122 @@ public class OpenActivity extends ListActivity
 		
 	}
 	
-	private void lookForRelevantFiles(File file, ArrayList<File> relevantFiles)
+	class DirFindAsyncTask extends AsyncTask<File, String, ArrayList<File>>
 	{
+		OpenActivity mm_activity;
 		
-		if(file.isDirectory())
+		public DirFindAsyncTask(OpenActivity activity)
 		{
-			File[] subdirs = file.listFiles(new FileFilter() 
+			mm_activity = activity;
+		}
+		
+		@Override
+		protected void onProgressUpdate (String... values)
+		{
+			m_progressText.setText(values[0]);
+		}
+		
+		@Override
+		protected void onPreExecute ()
+		{
+			m_progressText.setVisibility(View.VISIBLE);
+			m_progressWheel.setVisibility(View.VISIBLE);
+		}
+		
+		/**
+		 * Looks thru file system for wolf3d folders
+		 * @param file The root folder
+		 * @param relevantFiles The list to fill
+		 */
+		private void lookForRelevantFiles(File file, ArrayList<File> relevantFiles)
+		{
+			if(isCancelled())
+				return;
+			if(file.isDirectory())
 			{
-				@Override
-				public boolean accept(File pathname) 
+				publishProgress(file.getPath());
+				File[] subdirs = file.listFiles(new FileFilter() 
 				{
-					return pathname.isDirectory();
-				}
-			});
-			File[] wl6files = file.listFiles(new FilenameFilter() 
-			{
-				@Override
-				public boolean accept(File dir, String filename) 
-				{
-					for(String name : Global.s_wolfFileNames)
+					@Override
+					public boolean accept(File pathname) 
 					{
-						if(name.compareToIgnoreCase(filename) == 0)
-							return true;
+						return pathname.isDirectory();
 					}
-					return false;
-				}
-			});
+				});
+				File[] wl6files = file.listFiles(new FilenameFilter() 
+				{
+					@Override
+					public boolean accept(File dir, String filename) 
+					{
+						for(String name : Global.s_wolfFileNames)
+						{
+							if(name.compareToIgnoreCase(filename) == 0)
+								return true;
+						}
+						return false;
+					}
+				});
+				
+				if(wl6files.length == Global.s_wolfFileNames.length)
+					relevantFiles.add(file);
 			
-			if(wl6files.length == Global.s_wolfFileNames.length)
-				relevantFiles.add(file);
-			else
 				for(File subdir : subdirs)
 					lookForRelevantFiles(subdir, relevantFiles);
+			}
+		}
+		
+		@Override
+		protected ArrayList<File> doInBackground(File... params) 
+		{
+			ArrayList<File> dirList = new ArrayList<File>();
+			lookForRelevantFiles(params[0],	dirList);
+			return dirList;
+		}
+		
+		@Override
+		protected void onPostExecute (ArrayList<File> result)
+		{
+			m_adapter = new OpenAdapter(mm_activity, result.toArray(new 
+					File[result.size()]));
+			m_progressWheel.setVisibility(View.GONE);
+			m_progressText.setVisibility(View.GONE);
+			setListAdapter(m_adapter);
+			m_dataLoaded = true;
+			
+			Log.i("T", "Task finished");
+		}
+		
+		@Override
+		protected void onCancelled (ArrayList<File> result)
+		{
+			Log.i("T", "Task cancelled");
 		}
 	}
+	
 	
 	@Override
 	protected void onCreate (Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_open);
 		
 		m_inflater = (LayoutInflater)getSystemService(Context
 				.LAYOUT_INFLATER_SERVICE);
 		
-		updateList();
+		m_progressWheel = (ProgressBar)findViewById(R.id.progress_wheel);
+		m_progressText = (TextView)findViewById(R.id.progress_text);
+		
+		m_dirFindAsyncTask = new DirFindAsyncTask(this);
+		
+		if(!m_dataLoaded)
+			updateList();
+	}
+	
+	@Override
+	protected void onSaveInstanceState (Bundle outState)
+	{
+		m_dirFindAsyncTask.cancel(true);
+		super.onSaveInstanceState(outState);
 	}
 	
 	/**
@@ -151,16 +229,6 @@ public class OpenActivity extends ListActivity
 	 */
 	private void updateList()
 	{
-		ArrayList<File> dirList = new ArrayList<File>();
-		lookForRelevantFiles(Environment.getExternalStorageDirectory(),	
-				dirList);
-		m_adapter = new OpenAdapter(this, dirList.toArray(new File[dirList
-		                                                           .size()]));
-		setListAdapter(m_adapter);
+		m_dirFindAsyncTask.execute(Environment.getExternalStorageDirectory());
 	}
-	
-	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id)
-	{
-	}	
 }
