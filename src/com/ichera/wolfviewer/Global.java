99c1b9938ccfd55c3721127f14e784d641896f27
Ioan Chera <ioan_chera@yahoo.com>
2014-02-08 18:47:30 +0200

    Added level reading support

--- src/com/ichera/wolfviewer/Global.java
+++ src/com/ichera/wolfviewer/Global.java
@@ -109,4 +109,122 @@ public class Global
 		new AlertDialog.Builder(context).setTitle(title)
 		.setMessage(message).setNeutralButton("OK", null).show();
 	}
+	
+	private static final int NEARTAG = 0xa7;
+	private static final int FARTAG = 0xa8;
+	public static byte[] carmackExpand(byte[] source, int inOffset, int outLength)
+	{
+		byte[] dest = new byte[outLength];
+		int inIndex = inOffset;
+		int outIndex = 0;
+		int count;
+		int offset;
+		int copyIndex;
+		
+		int ch, chhigh;
+		
+		outLength /= 2;
+		while(outLength > 0)
+		{
+			ch = Global.readUInt16(source, inIndex);
+			inIndex += 2;
+			chhigh = ch / 256;
+			if(chhigh == NEARTAG)
+			{
+				count = ch & 0xff;
+				if(count == 0)
+				{
+					ch |= source[inIndex++];
+					dest[2 * outIndex] = (byte)(ch & 0xff);
+					dest[2 * outIndex + 1] = (byte)(ch / 256);
+					outIndex++;
+					outLength--;
+				}
+				else
+				{
+					offset = source[inIndex++];
+					copyIndex = outIndex - offset;
+					outLength -= count;
+					if(outLength < 0)
+						return dest;
+					while(count > 0)
+					{
+						dest[2 * outIndex] = dest[2 * copyIndex];
+						dest[2 * outIndex + 1] = dest[2 * copyIndex + 1];
+						outIndex++;
+						copyIndex++;
+						count--;
+					}
+				}
+			}
+			else if(chhigh == FARTAG)
+			{
+				count = ch & 0xff;
+				if(count == 0)
+				{
+					ch |= source[inIndex++];
+					dest[2 * outIndex] = (byte)(ch & 0xff);
+					dest[2 * outIndex + 1] = (byte)(ch / 256);
+					outIndex++;
+					outLength--;
+				}
+				else
+				{
+					offset = Global.readUInt16(source, inIndex);
+					inIndex += 2;
+					copyIndex = offset;
+					outLength -= count;
+					if(outLength < 0)
+						return dest;
+					while(count > 0)
+					{
+						dest[2 * outIndex] = dest[2 * copyIndex];
+						dest[2 * outIndex + 1] = dest[2 * copyIndex + 1];
+						outIndex++;
+						copyIndex++;
+						count--;
+					}
+				}
+			}
+			else
+			{
+				dest[2 * outIndex] = (byte)(ch & 0xff);
+				dest[2 * outIndex + 1] = (byte)(ch / 256);
+				outLength--;
+			}
+		}
+		
+		return dest;
+	}
+	
+	public static short[] rlewExpandByteToShort(byte[] source, int inOffset, int outLength, int rlewTag)
+	{
+		short[] dest = new short[outLength];
+		int inIndex = inOffset;
+		int outIndex = 0;
+		int value, count, i;
+		
+		do
+		{
+			value = readUInt16(source, inIndex);
+			inIndex += 2;
+			if(value != rlewTag)
+			{
+				dest[outIndex++] = (short)(value & 0xffff);
+			}
+			else
+			{
+				count = readUInt16(source, inIndex);
+				inIndex += 2;
+				value = readUInt16(source, inIndex);
+				inIndex += 2;
+				for(i = 0; i < count; ++i)
+				{
+					dest[outIndex++] = (short)(value & 0xffff);
+				}
+			}
+		}while(outIndex < dest.length);
+		
+		return dest;
+	}
 }
---
+++ src/com/ichera/wolfviewer/document/LevelContainer.java
@@ -0,0 +1,179 @@
+package com.ichera.wolfviewer.document;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import com.ichera.wolfviewer.Global;
+
+public class LevelContainer 
+{
+	private static final int NUMMAPS = 60;
+	private static final int MAPPLANES = 2;
+	private static final int MAPSIZE = 64;
+	private static final int maparea = MAPSIZE * MAPSIZE;
+	
+	private short[][][] mLevels;
+	
+	public short getTile(int level, int plane, int x, int y)
+	{
+		return mLevels[level][plane][x * MAPSIZE + y];
+	}
+	
+	public short getTile(int level, int plane, int i)
+	{
+		return mLevels[level][plane][i];
+	}
+	
+	public short[] getPlane(int level, int plane)
+	{
+		return mLevels[level][plane];
+	}
+	
+	public short[][] getLevel(int level)
+	{
+		return mLevels[level];
+	}
+	
+	public short[][][] getLevels()
+	{
+		return mLevels;
+	}
+	
+	public boolean loadFile(File mapHead, File gameMaps)
+	{
+		FileInputStream fis = null;
+		RandomAccessFile raf = null;
+		try
+		{
+			if(mapHead.isDirectory() || gameMaps.isDirectory())
+				return false;
+			if(mapHead.length() < 2 + 4 * NUMMAPS)
+				return false;
+			
+			int newRlewTag;
+			int[] headerOffsets = new int[NUMMAPS];
+			
+			fis = new FileInputStream(mapHead);
+			newRlewTag = Global.readUInt16(fis);
+			for(int i = 0; i < NUMMAPS; ++i)
+				headerOffsets[i] = Global.readInt32(fis);
+			
+			fis.close();
+			fis = null;
+			
+			raf = new RandomAccessFile(gameMaps, "r");
+			int pos;
+			MapHeader newHeader = new MapHeader();
+			
+			short[][][] newLevels = new short[maparea][][];
+			for(int i = 0; i < NUMMAPS; ++i)
+			{
+				pos = headerOffsets[i];
+				if(pos < 0)
+					continue;
+				raf.seek(pos);
+				newHeader = new MapHeader();
+				newHeader.planeStart[0] = Global.readInt32(raf);
+				newHeader.planeStart[1] = Global.readInt32(raf);
+				newHeader.planeStart[2] = Global.readInt32(raf);
+				newHeader.planeLength[0] = Global.readUInt16(raf);
+				newHeader.planeLength[1] = Global.readUInt16(raf);
+				newHeader.planeLength[2] = Global.readUInt16(raf);
+				raf.skipBytes(2 + 2 + 16);
+				
+				newLevels[i] = cacheMap(raf, newHeader, newRlewTag);
+				if(newLevels[i] == null)
+					return false;
+			}
+			// All ok
+			mLevels = newLevels;
+		}
+		catch(FileNotFoundException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		finally
+		{
+			if(fis != null)
+			{
+				try
+				{
+					fis.close();
+				}
+				catch(IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+			if(raf != null)
+			{
+				try
+				{
+					raf.close();
+				}
+				catch(IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+		return true;
+	}
+	
+	private short[][] cacheMap(RandomAccessFile raf, MapHeader newHeader, 
+			int newRlewTag) throws IOException
+	{
+		short[][] ret = new short[MAPPLANES][];
+		try
+		{
+			int pos, compressedLength, intermediaryLength, expandedLength;
+			byte[] compressedData = null;
+			byte[] intermediaryData;
+			for(int plane = 0; plane < MAPPLANES; ++plane)
+			{
+				pos = newHeader.planeStart[plane];
+				compressedLength = newHeader.planeLength[plane] - 2;
+				raf.seek(pos);
+				// First read the intermediary length, then the compressed data
+				intermediaryLength = Global.readUInt16(raf);
+				if(compressedData == null || compressedData.length < compressedLength)
+					compressedData = new byte[compressedLength];
+				raf.read(compressedData, 0, compressedLength);
+				
+				// Compressed data -> Carmack Expand -> Intermediary data
+				intermediaryData = Global.carmackExpand(compressedData, 0, intermediaryLength);
+				expandedLength = (intermediaryData[0] & 0xff) + (intermediaryData[1] & 0xff) * 256;
+				
+				// Intermediary data -> RLEW Expand -> Final data
+				ret[MAPPLANES] = Global.rlewExpandByteToShort(intermediaryData, 2, 
+						expandedLength, newRlewTag);
+			}
+		}
+		catch(NegativeArraySizeException e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+		catch(IndexOutOfBoundsException e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+		return ret;
+	}
+	
+	private class MapHeader
+	{
+		int planeStart[] = new int[3];
+		int planeLength[] = new int[3];
+	}
+}
