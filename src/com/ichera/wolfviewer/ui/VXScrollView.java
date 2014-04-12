993d3eedd202db90d26e6230d13793a0e1c3ee39
Ioan Chera <ioan_chera@yahoo.com>
2014-04-12 20:11:56 +0300

    Revert "Thank you Internet. I think I solved the scroller vs button dillema, with a much simpler code."
    This reverts commit 284f61389cd1e62119ba678f58fbaa3f2ec0b49b.

--- src/com/ichera/wolfviewer/ui/VXScrollView.java
+++ src/com/ichera/wolfviewer/ui/VXScrollView.java
@@ -25,6 +25,36 @@ import android.widget.ScrollView;
 
 public class VXScrollView extends ScrollView 
 {
+	private boolean mScrollable = true;
+	
+	public void setScrollingEnabled(boolean enabled) {
+        mScrollable = enabled;
+    }
+
+    public boolean isScrollable() {
+        return mScrollable;
+    }
+    
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                // if we can scroll pass the event to the superclass
+                if (mScrollable) return super.onTouchEvent(ev);
+                // only continue to handle the touch event if scrolling enabled
+                return mScrollable; // mScrollable is always false at this point
+            default:
+                return super.onTouchEvent(ev);
+        }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        // Don't do anything with intercepted touch events if 
+        // we are not scrollable
+        if (!mScrollable) return false;
+        else return super.onInterceptTouchEvent(ev);
+    }
 	
     private ScrollViewListener scrollViewListener = null;
 
@@ -60,9 +90,5 @@ public class VXScrollView extends ScrollView
     	{
     		scrollViewListener.onOverScrolled(this, scrollX, scrollY, clampedX, clampedY);
     	}
-    }
-    
-    public boolean onTouchEvent(MotionEvent ev) {
-        return false;
     }
 }
